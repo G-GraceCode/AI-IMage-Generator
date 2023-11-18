15453f0e5d13106d691aa5dd85f1e7466ad5b8f8
Tandu Yanmick Ateh <95031570+G-GraceCode@users.noreply.github.com>
2023-11-18 19:07:33 +0000

    Update 5 files

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -15,19 +15,23 @@ const CreatePost = () => {
   const [geneImg, setGeneImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const generateImage = () => {};
+
   const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+  const handleSurpriseMe = (e) => {
     e.preventDefault()
-  }
-const handleChange = (e) => {
-  const {name, value} = e.target
-  setForm({
-    ...form,
-    [name]: value,
-  })
-}
-const handleSurpriseMe = () => {
-  
-}
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt})
+  };
   return (
     <Section className="max-w-7xl max-auto">
       <div>
@@ -41,25 +45,69 @@ const handleSurpriseMe = () => {
       </div>
 
       <form>
-      <div>
-        <FormField 
-        labelName="Your name"
-        type="text"
-        name='name'
-        placeholder='eg, John'
-        value={form.name}
-        handleChange={handleChange}
-        />
-        <FormField 
-        labelName="prompt"
-        type="text"
-        name='prompt'
-        placeholder='eg, John'
-        value={form.prompt}
-        handleChange={handleChange}
-        isSurpriseMe
-        />
-      </div>
+        <div>
+          <FormField
+            labelName="Your name"
+            type="text"
+            name="name"
+            placeholder="eg, John"
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField
+            labelName="prompt"
+            type="text"
+            name="prompt"
+            placeholder="like.. panda mad scientist mixing sparkling chemicals, digital arteg, John"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+
+          <div className="relative bg-gray-50 border border-grey-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+            {form.photo ? (
+              <img
+                src={form.photo}
+                alt={form.prompt}
+                className="w-full h-full object-contain"
+              />
+            ) : (
+              <img
+                src={preview}
+                alt="prompt"
+                className="w-9/12 h-9/12 object-contain opacity-40"
+              />
+            )}
+
+            {geneImg && (
+              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg]">
+                <Loader />
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="mt-10">
+          <button
+            type="button"
+            onClick={generateImage}
+            className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {geneImg ? "Generating..." : "Generate"}
+          </button>
+        </div>
+        <div className="mt-1o">
+          <p className="mt-2 text-[#666e75] text-[14px]">
+            Once you have created the image, you can share it with other in the
+            community
+          </p>
+          <button
+            type="submit"
+            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {loading ? "Sharing..." : "Share with the community."}
+          </button>
+        </div>
       </form>
     </Section>
   );
