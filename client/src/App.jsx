1cab9af74189f5e367e7db1a993341cc33c9d5c2
Tandu Yanmick Ateh <95031570+G-GraceCode@users.noreply.github.com>
2023-12-28 09:23:04 +0000

    Update 3 files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import "./index.css";
-import { Home, CreatePost } from "./pages/Pages";
+import { Home, CreatePost, Signup } from "./pages/Pages";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Header />
-      <main className="sm:p-8 px-4 py-8 w-full  bg-[#000]  min-h-[calc(100vh-73px)]">
-        {" "}
+      <main className="sm:p-8 px-4 py-8 w-full  bg-[#000] ">
+        <Header />
         {/*bg-[#f9fafe] */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </main>
     </BrowserRouter>
---
+++ client/src/pages/Signup.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+
+const Signup = ()=> {
+    return(
+<>Sign up</>
+    )
+}
+export default Signup
