359bca3a432e6b0da4de953163d0db1c18874319
Tandu Yanmick Ateh <95031570+G-GraceCode@users.noreply.github.com>
2023-12-29 09:40:58 +0000

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,7 +7,9 @@ const RenderCards = ({ data, title }) => {
     return data.map((post) => <Card key={post._id} {...post} />);
   }
   return (
-    <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">{title}</h2>
+    <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase text-center">
+      {title}
+    </h2>
   );
 };
 
