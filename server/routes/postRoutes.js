ffedd4e3e87ee6e6a4b1b8777dfad1f973dbff6f
Tandu Yanmick Ateh <95031570+G-GraceCode@users.noreply.github.com>
2023-12-29 10:58:00 +0000

    Update .env and postRoutes.js

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -7,4 +7,45 @@ import Post from "../mongoDB/models/post.js";
 
 const router = express.Router();
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// route to get from server, database
+router.route("/posts").get(async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.status(200).json({ success: true, data: posts });
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      message: "Fetching posts failed, please try again",
+    });
+  }
+});
+
+// route to post data to the server
+router.route("/createpost").post(async (res, req) => {
+  try {
+    const { name, prompt, photo } = req.body;
+    const photoUrl = await cloudinary.uploader.upload(photo);
+    const newPost = await Post.create({
+      name,
+      prompt,
+      photo: photoUrl.url,
+    });
+    res.status(200).json({
+      success: true,
+      data: newPost,
+    });
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      message: "Unable to create a post, please try again",
+    });
+  }
+});
+
 export default router;
